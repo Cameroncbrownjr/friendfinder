2255b097f6b9b611430aca75afe65a19044d5909
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-09-02 17:02:53 -0400

    testing post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,5 +2,8 @@ var friends = require("../data/friend.js");
 
 
 module.exports = function (app) {
-    app.get('/friends', (req, res) => res.json(friends))
+    app.get('/friends', (req, res) => res.json(friends));
+    app.post("/friends", (req, res) => {
+        console.log(req.body)
+    });
 };
