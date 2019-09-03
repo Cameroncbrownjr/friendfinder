c0bd492c1b490819e488116ee5d444067a67379e
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-09-03 02:48:43 -0400

    missing the readme

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,18 +2,18 @@ module.exports =
     [
         {
             name: " Justin Bieber",
-            photo: "https://pbs.twimg.com/profile_images/898295311893880832/bCps4HFV_400x400.jpg",
-            scores: [5, 2, 5, 3, 1, 3, 4, 5, 3, 2]
+            picture: "https://pbs.twimg.com/profile_images/898295311893880832/bCps4HFV_400x400.jpg",
+            questions: [5, 2, 5, 3, 1, 3, 4, 5, 3, 2]
         },
         {
             name: "Grace Potter",
-            photo: "http://soundsummit.net/wp-content/uploads/2018/07/grace2018.jpg",
-            scores: [2, 3, 2, 1, 4, 5, 5, 2, 4, 1]
+            picture: "http://soundsummit.net/wp-content/uploads/2018/07/grace2018.jpg",
+            questions: [2, 3, 2, 1, 4, 5, 5, 2, 4, 1]
         },
         {
             name: "Tina Halladay",
-            photo: "https://blog.thecurrent.org/wp-content/blogs.dir/9/files/2018/05/Tina-Halladay-Sheer-Mag.jpg",
-            scores: [1, 4, 2, 5, 2, 1, 5, 3, 3, 4]
+            picture: "https://blog.thecurrent.org/wp-content/blogs.dir/9/files/2018/05/Tina-Halladay-Sheer-Mag.jpg",
+            questions: [1, 4, 2, 5, 2, 1, 5, 3, 3, 4]
         }
     ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,30 @@ var friends = require("../data/friend.js");
 
 module.exports = function (app) {
     app.get('/friends', (req, res) => res.json(friends));
+
     app.post("/friends", (req, res) => {
         var friend = {}
         friend.name = req.body.name
         friend.picture = req.body.picture
         friend.questions = req.body.questions.map(i => parseInt(i))
-        console.log(friend)
-        friends.push(friend)
+
+
+        var lastDiference = Infinity;
+        var match = {};
+        for (i in friends) {
+            var difference = 0;
+            for (j in friends[i].questions) {
+                difference += Math.abs(parseInt(friends[i].questions[j]) - parseInt(friend.questions[j]));
+            };
+            if (difference < lastDiference) {
+                lastDiference = difference
+                match = friends[i]
+            }
+        };
+        friends.push(friend);
+        res.json(match);
+
     });
+
 };
+
