e2dc7598a318703f9f256cb907b7b61c466fcb23
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-09-03 01:01:45 -0400

    updated survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,6 +4,11 @@ var friends = require("../data/friend.js");
 module.exports = function (app) {
     app.get('/friends', (req, res) => res.json(friends));
     app.post("/friends", (req, res) => {
-        console.log(req.body)
+        var friend = {}
+        friend.name = req.body.name
+        friend.picture = req.body.picture
+        friend.questions = req.body.questions.map(i => parseInt(i))
+        console.log(friend)
+        friends.push(friend)
     });
 };
